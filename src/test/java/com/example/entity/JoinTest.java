86aa7f72141db38eed22ab0a402e966903d190fb
bennixview <benjamin.wirtz@gmail.com>
2014-08-21 09:25:37 +0200

    Test now use Hamcrest

--- src/test/java/com/example/entity/JoinTest.java
+++ src/test/java/com/example/entity/JoinTest.java
@@ -1,13 +1,17 @@
 package com.example.entity;
 
 import com.mysema.query.jpa.impl.JPAQuery;
+
 import org.junit.Before;
 import org.junit.Test;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.List;
-import static junit.framework.Assert.assertTrue;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 
 public class JoinTest {
 
@@ -34,21 +38,21 @@ public class JoinTest {
         }
 
         // EntityA 1--n EntityB n--1 EntityC
-
-
         em.flush();
         em.clear();
 
-        QEntityA qEntityA = new QEntityA(EntityA.class.getSimpleName());
-        QEntityB qEntityB = new QEntityB(EntityB.class.getSimpleName());
-        QEntityC qEntityC = new QEntityC(EntityC.class.getSimpleName());
+        QEntityA qEntityA = QEntityA.entityA;
+        QEntityB qEntityB = QEntityB.entityB;
+        QEntityC qEntityC = QEntityC.entityC;
 
         List<EntityA> list = new JPAQuery(em)
                 .from(qEntityA)
                 .leftJoin(qEntityA.entityBs, qEntityB)
-                .innerJoin(qEntityB.entityC, qEntityC)
+                .innerJoin(qEntityB.entityC, qEntityC)     //   <<--- Is it possible to make a nested inner Join here
                 .on(qEntityC.value.eq("-5"))
                 .list(qEntityA);
+
+        assertThat(list.size(), is(1));
 
 
         /*I want to say exactly that:
@@ -72,8 +76,9 @@ public class JoinTest {
         */
 
 
-        assertTrue(list.size() == 1);
+
 
 
     }
+
 }
